064071e0554244fcf60ab0c674cb494ad3447ca2
Sulman Khan <sulman@vt.edu>
2025-01-08 15:33:54 -0500

    updated portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -255,8 +255,8 @@ const achievementSection = {
       subtitle: "The sheer volume of data on Reddit makes it challenging to manually track and understand the key themes and sentiments within various subreddits. This project addresses the need for an automated system that can efficiently collect, process, and analyze Reddit data to extract meaningful insights.   ",
       image: require("./assets/images/reddit_logo.png"),
       footerLink: [
-    { name: "Github: On-Prem", url: "https://github.com/SulmanK/reddit_ai_pulse_local_public"},
-        { name: "Blog: Building an AI-Powered Data Pipeline", url: "https://sulmank.github.io/Blog/writing/2025/01/08/Reddit%20AI%20Pulse%20%28ON-PREM%29/" },
+    { name: "Github: On-Prem Implementation", url: "https://github.com/SulmanK/reddit_ai_pulse_local_public"},
+        { name: "Blog: Building an AI-Powered Data Pipeline (On-Prem)", url: "https://sulmank.github.io/Blog/writing/2025/01/08/Reddit%20AI%20Pulse%20%28ON-PREM%29/" },
         { name: "Website: Reddit AI Pulse", url: "https://reddit-text-insight-and-sentiment-website-local.vercel.app/"}
       ]
     },
